78cf7aef1f7c133ea0c8959e369ffa3b818c0394
MiyamotoAkira <yefo.akira@gmail.com>
2015-10-26 18:46:40 +0000

    Uglify The source file
    Changed lines, tabs, and such to achieve a more ugly version of the code that is more difficult to read.

--- EightQueens/EightQueensLogic/MyClass.cs
+++ EightQueens/EightQueensLogic/MyClass.cs
@@ -11,7 +11,6 @@ namespace EQL
 
         public List<Tuple<int,int>> Solve()
         {
-            
             int[,] b = new int[8,8];
             for (int i = 0; i < 8; i++)
             {
@@ -20,34 +19,22 @@ namespace EQL
                     b[i, j] = 0;
                 }
             }
-
             var starter = 0;
             for (int i = 0; i < 8; i++)
             {
-
                 var f = false;
                 for (int j = starter; j < 8; j++)
                 {
                     if (b[i,j] == 0)
                     {
-                        f = true;
-                        b[i, j] = 1;
-                        for (int k = 0; k < 8; k++)
-                        {
-                            if (b[i, k] != 1)
-                            {
+      f = true;
+             b[i, j] = 1;
+                        for (int k = 0; k < 8; k++){
+                        if (b[i, k] != 1){
                                 b[i,k] = 2;
-                            }
-                        }
-
-                        for (int l = 0; l < 8; l++)
-                        {
-                            if (b[l,j] != 1)
-                            {
-                                b[l, j] = 2;
-                            }
-                        }
-
+                            }}
+for (int l = 0; l < 8; l++)
+{if (b[l,j] != 1){b[l, j] = 2;}}
                         for (int k = j, l = i; k<8 && l < 8; k++, l++)
                         {
                             if (b[l,k] != 1)
@@ -55,53 +42,43 @@ namespace EQL
                                 b[l, k] = 2;
                             }
                         }
-
                         for (int k = j, l = i; k<8 && l >= 0; k++, l--)
                         {
-                            if (b[l, k] != 1)
-                            {
+                        if (b[l, k] != 1)
+                         {
                                 b[l, k] = 2;
                             }
-                        }
-
-
-                        for (int k = j, l = i; k >=0 && l < 8; k--, l++)
+           
+                       }
+                   for (int k = j, l = i; k >=0 && l < 8; k--, l++)
                         {
                             if (b[l,k] != 1)
                             {
-                                b[l, k] = 2;
+                        b[l, k] = 2;
                             }
                         }
-
-                        for (int k = j, l = i; k >= 0 && l >= 0; k--, l--)
-                        {
-                            if (b[l,k] != 1)
+            for (int k = j, l = i; k >= 0 && l >= 0; k--, l--)
+            {
+                if (b[l,k] != 1)
                             {
                                 b[l, k] = 2;
                             }
                         }
-
                         break;
                     }
                 }
-
                 if (!f)
                 {
-
                     i = i-1;
-
-
                     for (int j = 0; j < 8; j++)
                     {
-                        if (b[i, j] == 1)
-                        {
-                            b[i, j] = 0;
-                            starter = j + 1;
-                        }
-                    }
-
+if (b[i, j] == 1)
+{
+b[i, j] = 0;
+starter = j + 1;
+}
+                }
                     i = i-1;
-
                     for (int k = 0; k < 8; k++)
                     {
                         for (int j = 0; j < 8; j++)
@@ -112,62 +89,55 @@ namespace EQL
                             }
                         }
                     }
-
                     for (int x = 0; x < 8; x++)
                     {
                         for (int y = 0; y < 8; y++)
                         {
-                            if (b[x, y] == 1)
-                            {
-                                for (int k = 0; k < 8; k++)
-                                {
-                                    if (b[x, k] != 1)
-                                    {
+            if (b[x, y] == 1)
+            {
+                for (int k = 0; k < 8; k++)
+                {
+                    if (b[x, k] != 1)
+                    {
                                         b[x, k] = 2;
-                                    }
                                 }
-
-                                for (int l = 0; l < 8; l++)
+                            }
+                            for (int l = 0; l < 8; l++)
                                 {
                                     if (b[l, y] != 1)
-                                    {
-                                        b[l, y] = 2;
-                                    }
-                                }
-
-                                for (int k = y, l = x; k < 8 && l < 8; k++, l++)
-                                {
-                                    if (b[l, k] != 1)
+                {
+                    b[l, y] = 2;
+                }
+            }
+            for (int k = y, l = x; k < 8 && l < 8; k++, l++)
+            {
+                if (b[l, k] != 1)
                                     {
                                         b[l, k] = 2;
                                     }
                                 }
-
                                 for (int k = y, l = x; k < 8 && l >= 0; k++, l--)
                                 {
-                                    if (b[l, k] != 1)
-                                    {
-                                        b[l, k] = 2;
+            if (b[l, k] != 1)
+            {
+                b[l, k] = 2;
+            }
+        }
+        for (int k = y, l = x; k >= 0 && l < 8; k--, l++)
+        {
+            if (b[l, k] != 1)
+            {
+                b[l, k] = 2;
                                     }
                                 }
-
-
-                                for (int k = y, l = x; k >= 0 && l < 8; k--, l++)
-                                {
-                                    if (b[l, k] != 1)
-                                    {
-                                        b[l, k] = 2;
-                                    }
-                                }
-
                                 for (int k = y, l = x; k >= 0 && l >= 0; k--, l--)
                                 {
                                     if (b[l, k] != 1)
                                     {
-                                        b[l, k] = 2;
-                                    }
-                                }
-                            }
+                b[l, k] = 2;
+            }
+        }
+    }
                         }
                     }
 
@@ -181,6 +151,14 @@ namespace EQL
                     starter = 0;
                 }
             }
+
+
+
+
+
+
+
+
 
             List<Tuple<int,int>> result = new List<Tuple<int,int>>();
             for (int i = 0; i <  8; i++)
